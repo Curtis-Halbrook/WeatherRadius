f3107abae6427e789cc10df57d36428aed32c1b6
Curtis Halbrook <calbrook@us.ibm.com>
2023-12-21 14:22:01 -0500

    Initial

---
+++ WeatherRadius/WeatherRadius/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherRadius
+//
+//  Created by Curtis Halbrook on 12/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherRadius/WeatherRadius/WeatherRadiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherRadiusApp.swift
+//  WeatherRadius
+//
+//  Created by Curtis Halbrook on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherRadiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
